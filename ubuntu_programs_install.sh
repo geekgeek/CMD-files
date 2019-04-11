50c55e6cff56137aff442de5a7261fd67c9bcfab
geekgeek <solq87@gmail.com>
2019-04-12 01:20:45 +0200

    Create ubuntu_programs_install.sh

---
+++ ubuntu_programs_install.sh
@@ -0,0 +1,16 @@
+#Ubuntu programs installer script
+##############################
+##############################
+##############################
+
+
+#htop
+sudo apt-get install htop -y
+
+
+#vim
+sudo apt-get install vim -y
+
+
+#i3 window manager
+sudo apt-get install i3 -y
