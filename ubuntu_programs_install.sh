45efb8276bf755e57ae88fd19f6a5249a86eadbc
geekgeek <solq87@gmail.com>
2020-06-04 21:12:25 +0200

    Update ubuntu_programs_install.sh

--- ubuntu_programs_install.sh
+++ ubuntu_programs_install.sh
@@ -15,10 +15,13 @@ sudo apt-get install vim -y
 #i3 window manager
 sudo apt-get install i3 -y
 
+#tmux
+apt-get install tmux
+
 #latex
-sudo apt-get install texlive-full -y
+#sudo apt-get install texlive-full -y
 
 #python jupyter notebook
-apt-get install python3-pip -y
-python3 -m pip install jupyter
+#apt-get install python3-pip -y
+#python3 -m pip install jupyter
 
